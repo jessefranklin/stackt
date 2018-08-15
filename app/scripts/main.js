81a66df7504ce3c4c2d299aa0d3a18d867013efd
jessefranklin <jessefrankling@gmail.com>
2018-08-14 21:51:46 -0400

    Copy font size updates, video ios, stackt logo masking refactor

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -14,70 +14,49 @@ $(document).ready(function(){
         once: false
       });
 
-
-    blackmask();
-
     setTimeout(function(){
-      $('body,html').css({'overflow-y':'auto'});
-    },1000);
+      blackmask();
+    },250);
 
-    var logoM = 80;
-
-    if(Modernizr.mq('(max-width: 990px)')){
-      logoM = 100;
-    }
-
-    var $header = $('body').find('.stackt-discover'),
-        $sticky = $('body').find('.sticky');
-
-    $(window).on('scroll', function() {
-      var top = $('html').scrollTop() || $('body').scrollTop();
-      if(top > $header.offset().top+logoM){
-        $sticky.addClass('show');
-      } else {
-        $sticky.removeClass('show');
-      }
-    });
+  
   });
 
   $(window).on('resize',function(){
 
     clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(function() {   
-
-      $('.brand-logo-black').each(function(i, obj) {
-        var el = $(this);
-        var t = $(this).closest('section');
-        var topContainer = t.offset().top;
-        var top = (window.pageYOffset || document.scrollTop) - topContainer;
-        el.css({top: top});
-        $(window).on('scroll', function() {
-          var top = (window.pageYOffset || document.scrollTop) - topContainer;
-          if(!el.hasClass('show')){
-            el.css({top: top});
-          }
-        });
+    resizeTimer = setTimeout(function() {  
+      $('.mask-fixed').css({'height':'auto','position':'relative'});
+      $('.mask-logo-blk').each(function(i, obj) {
+        var x = $(this).height();
+        $(this).height(x);
+        $(this).find('.mask-fixed').css({'height':x,'position':'absolute'});
       });
 
     }, 250);
     
   });
   
+
+   var $header = $('body').find('.stackt-discover'),
+        $sticky = $('.stackt-discover').find('.brand-logo-black');
+
+    $(window).on('scroll', function() {
+      var top = $('html').scrollTop() || $('body').scrollTop();
+      if(top > $header.offset().top){
+        $sticky.addClass('show');
+      } else {
+        $sticky.removeClass('show');
+      }
+    });
+
 });
 
 
 function blackmask(){
   var doc = document.documentElement;
-  $('.brand-logo-black').each(function(i, obj) {
-    var el = $(this);
-    var t = $(this).closest('section');
-    var topContainer = t.offset().top;
-    $(this).css('opacity',1);
-    $(window).on('scroll', function() {
-      var top = (window.pageYOffset || doc.scrollTop) - topContainer;
-      if(!el.hasClass('show')){
-        el.css({top: top});
-      }
-    });
+  $('.mask-logo-blk').each(function(i, obj) {
+      var x = $(this).height();
+      $(this).height(x);
+      $(this).find('.mask-fixed').css({'height':x,'position':'absolute'});
   });
 }
